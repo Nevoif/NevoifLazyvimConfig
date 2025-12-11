7a08b3e64851c22a558f107dca8ccb477a80b793
nevoif <nevoif@hotmail.com>
2025-12-11 18:08:26 +0300

    extras and mason installs are downloaded automatically

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -17,8 +17,30 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import/override with your plugins
+    { "LazyVim/LazyVim",                                     import = "lazyvim.plugins" },
+
+    -- 1. Languages
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+
+    -- 2. Coding & AI
+    { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
+    { import = "lazyvim.plugins.extras.coding.blink" },
+
+    -- 3. Editor & UI
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.editor.dial" },
+    { import = "lazyvim.plugins.extras.editor.neo-tree" },
+    { import = "lazyvim.plugins.extras.editor.telescope" },
+    { import = "lazyvim.plugins.extras.util.dot" },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+    { import = "lazyvim.plugins.extras.vscode" },
+
+    -- 4. Import/override with your custom plugins (e.g., roslyn.lua, mason.lua)
     { import = "plugins" },
   },
   defaults = {
@@ -34,7 +56,7 @@ require("lazy").setup({
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
-  }, -- automatically check for plugin updates
+  },                -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,9 +3,39 @@ return {
     "mason-org/mason.nvim",
     config = function()
       require("mason").setup {
+        -- Preserve your custom registries
         registries = {
           "github:Crashdummyy/mason-registry",
           "github:mason-org/mason-registry",
+        },
+        -- Force Mason to install these binaries automatically
+        ensure_installed = {
+          -- Core .NET / C#
+          "roslyn",
+          "netcoredbg",
+          "csharpier",
+          "lemminx", -- XML/csproj support
+          "powershell-editor-services",
+
+          -- Web / Front-end
+          "html-lsp",
+          "css-lsp",
+          "json-lsp",
+          "typescript-language-server",
+          "prettier",
+
+          -- DevOps / Config
+          "docker-compose-language-service",
+          "dockerfile-language-server",
+          "yaml-language-server",
+          "bash-language-server",
+          "black",           -- Python formatter (required by your list)
+          "tree-sitter-cli", -- Utility (required by your list)
+          "sqlfluff",        -- SQL formatter (recommended for lang.sql extra)
+
+          -- Config Maintenance / Utility
+          "marksman",
+          "lua-language-server",
         },
       }
     end,
