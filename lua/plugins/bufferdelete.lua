6d138445d9a09c9c85a824c0514c62be8171818f
nevoif <nevoif@hotmail.com>
2025-12-30 15:55:45 +0300

    pluging keybind fix and satelite.lua commented out

--- lua/plugins/bufferdelete.lua
+++ lua/plugins/bufferdelete.lua
@@ -1,11 +1,11 @@
 return {
-  "famiu/bufdelete.nvim",
-  cmd = { "Bdelete", "Bwipeout" },
-  keys = {
-    {
-      "<leader>x",
-      "<cmd>Bdelete<cr>",
-      desc = "Delete buffer (keep layout)",
+    "famiu/bufdelete.nvim",
+    cmd = { "Bdelete", "Bwipeout" },
+    keys = {
+        {
+            "<leader>bd",
+            "<cmd>Bdelete<cr>",
+            desc = "Delete buffer (keep layout)",
+        },
     },
-  },
 }
