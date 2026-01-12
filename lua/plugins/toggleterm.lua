8e4f19b0a534987c17d50bd7fc7ace7b6a126244
Nevoif <nevoif@hotmail.com>
2026-01-12 17:03:54 +0300

    last update's fixes

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,17 +1,1 @@
-return {
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    keys = {
-      { "<leader>`", "<cmd>ToggleTerm<CR>", desc = "Terminal: Toggle" },
-    },
-    config = function()
-      require("toggleterm").setup({
-        size = 20,
-        open_mapping = [[<leader>`]],
-        direction = "horizontal",
-        autochdir = true,
-      })
-    end,
-  },
-}
+--deleted
