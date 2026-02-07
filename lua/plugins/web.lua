80f66c264130451790117871740b175a94792fe7
Nevoif <nevoif@hotmail.com>
2026-02-07 08:32:00 +0300

    nvim-ts-autotag was giving errors, fixed the issue

--- lua/plugins/web.lua
+++ lua/plugins/web.lua
@@ -3,11 +3,15 @@ return {
     "windwp/nvim-ts-autotag",
     ft = { "html", "xml", "jsx", "tsx", "javascript", "typescript", "typescriptreact", "vue" },
     lazy = true,
-    opts = {
-      enable_close = true,
-      enable_rename = true,
-      enable_close_on_slash = false,
-    },
+    config = function()
+      require("nvim-ts-autotag").setup({
+        opts = {
+          enable_close = true,
+          enable_rename = true,
+          enable_close_on_slash = false,
+        },
+      })
+    end,
   },
 
   {
